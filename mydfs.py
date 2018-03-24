83caa288c453481476f08f7a7c58cf1cf2f7f00d
notEvil <a_rappold@gmx.at>
2018-03-24 09:02:43 +0100

    - refactored and documented second parts

--- mydfs.py
+++ mydfs.py
@@ -2,13 +2,12 @@
 TODO
 '''
 
-# TODO
 import fuse
-import os
-import logging
-import threading
-import stat
 import boltons.funcutils
+import os
+import stat
+import threading
+import collections
 
 ospath = os.path
 
@@ -80,6 +79,17 @@ class Mydfs(fuse.LoggingMixIn, fuse.Operations):
 
     @fuse_errors
     def create(self, path, mode, fi=None):
+        '''
+        Creates and opens file.
+
+        - for all paths
+          - creates parent directories as needed
+          - creates and opens file
+        - if any error
+          - for all files opened in reversed order
+            - closes file
+          - raises error
+        '''
         fileHandles = []
         try:
             for _, p in reversed(self._resolve(path, orBestInexistent=True)):
@@ -124,10 +134,10 @@ class Mydfs(fuse.LoggingMixIn, fuse.Operations):
         This might not be necessary.
         '''
         for _, p in reversed(self._resolve(path)):
-            stat = os.lstat(p)
+            stat_ = os.lstat(p)
 
         return {
-            key: getattr(stat, key)
+            key: getattr(stat_, key)
             for key in ('st_mode', 'st_ino', 'st_dev', 'st_nlink', 'st_uid', 'st_gid', 'st_size', 'st_atime',
                         'st_mtime', 'st_ctime', 'st_atime_ns', 'st_mtime_ns', 'st_ctime_ns')
         }
@@ -139,212 +149,200 @@ class Mydfs(fuse.LoggingMixIn, fuse.Operations):
     @fuse_errors
     def link(self, target, source):
         '''
-        - hard link
-        - target without source -> file not found error
-        - target exists -> file exists error
-        - source without target -> ignored
+        Creates hard link.
+
+        For details see `._link`.
         '''
-        sources = dict(self._resolve(source))
-
-        try:
-            targets = self._resolve(target)
-        except fuse.FuseOSError:  # targets not specified or don't exist
-            targets = {root: root + target for root in sources}
-        else:
-            targets = dict(targets)
-        # len(targets) != 0
-
-        if len(targets.keys() - sources.keys()) != 0:
-            raise fuse.FuseOSError(fuse.ENOENT)
-        # targets is subset of sources
-
-        for _, path in targets:
-            if ospath.exists(path):
-                raise fuse.FuseOSError(fuse.EEXIST)
-        # no target exists
-
-        for root, targetPath in targets.items():
-            self._ensure_directory(targetPath)
-            r = os.link(sources[root], targetPath)
-        # random order!
-
-        return r
-
-        if False:
-            '''
-            wrong but probably useful in the future
-            '''
-            sources = dict(self._resolve(source))
-            targets = dict(self._resolve(target, orBestInexistent=True))
-            # targets either exist
-            # or len() == 1
-
-            # potential overwrite
-            for root in sources.keys() & targets.keys():
-                r = os.link(sources[root], targets[root])
-
-            # create same root
-            roots = sources.keys() - targets.keys()
-            if len(roots) != 0:
-                root, t = next(iter(targets.items()))
-                t = t[len(root):]
-
-                for root in roots:
-                    path = root + t
-                    os.makedirs(ospath.dirname(path), exist_ok=True)
-                    r = os.link(sources[root], path)
-
-            # delete different root
-            for root in targets.keys() - sources.keys():
-                os.unlink(targets[root])
-
-            return r
+        return self._link(target, source, os.link)
 
     listxattr = None  # TODO see getxattr
 
     @fuse_errors
     def mkdir(self, path, mode):
-        for _, sPath in reversed(self._resolve(path, orBestInexistent=True)):
-            self._ensure_directory(sPath)
-            r = os.mkdir(sPath, mode)
+        '''
+        Creates directory.
+
+        - for all paths
+          - creates parent directories as needed
+          - creates directory
+        '''
+        for _, p in reversed(self._resolve(path, orBestInexistent=True)):
+            self._ensure_directory(p)
+            r = os.mkdir(p, mode)
+
         return r
 
     @fuse_errors
     def mknod(self, path, mode, dev):
-        for _, sPath in reversed(self._resolve(path), orBestInexistent=True):
-            self._ensure_directory(sPath)
-            r = os.mknod(sPath, mode, dev)
+        for _, p in reversed(self._resolve(path), orBestInexistent=True):
+            self._ensure_directory(p)
+            r = os.mknod(p, mode, dev)
+
         return r
 
     @fuse_errors
     def open(self, path, flags):
-        paths = reversed(self._resolve(path, orBestInexistent=True))
-        fhs = []
+        '''
+        Opens file.
+
+        - for all paths
+          - opens file
+        - if error
+          - for all files opened in reverse order
+            - closes file
+          - raises error
+        '''
+
+        fileHandles = []
         try:
-            for _, sPath in paths:
-                fh = os.open(sPath, flags)
-                fhs.append(fh)
-        except:
-            for fh in reversed(fhs):
-                os.close(fh)
+            for _, p in reversed(self._resolve(path, orBestInexistent=True)):
+                fileHandle = os.open(p, flags)
+                fileHandles.append(fileHandle)
+
+        except Exception:
+            for fileHandle in reversed(fileHandles):
+                os.close(fileHandle)
+
             raise
 
-        r = fhs[-1]
-        self._OpenFileHandles[r] = fhs
+        r = fileHandles[-1]
+        self._OpenFileHandles[r] = fileHandles
         return r
 
-    # def opendir(self, path):
+    # def opendir(self, path):  # residual from FUSE doc
 
     @fuse_errors
-    def read(self, path, size, offset, fh):
-        with self._get_file_handle_lock(fh):
-            os.lseek(fh, offset, 0)
-            r = os.read(fh, size)
+    def read(self, path, size, offset, fileHandle):
+        '''
+        Read from file.
+
+        - acquires lock
+        - sets position in file
+        - reads from file
+        - gets position in file
+        - for all files opened
+          - sets position in file
+        '''
+
+        with self._get_file_handle_lock(fileHandle):
+            os.lseek(fileHandle, offset, 0)  # from fusepy loopback example
+            r = os.read(fileHandle, size)
 
             # TODO could be very wrong
-            nOffset = os.lseek(fh, 0, os.SEEK_CUR)
-            for fhi in self._OpenFileHandles[fh]:
-                os.lseek(fhi, nOffset, 0)
+            nOffset = os.lseek(fileHandle, 0, os.SEEK_CUR)
+            for nFileHandle in self._OpenFileHandles[fileHandle]:
+                os.lseek(nFileHandle, nOffset, 0)
+
         return r
 
     @fuse_errors
     def readdir(self, path, fh):
+        '''
+        List directory.
+
+        - for all directories
+          - lists directory
+          - for all names
+            - if is directory
+              - adds name to result
+            - else
+              - remembers file id
+        - for all file ids
+          - builds mask
+          - builds and adds name to result
+
+        file id = (name, modification time, size)
+        '''
+
         # TODO check performance
 
         r = set()
-        allObjs = set()
-        objss = []
+        fileIds = []
+        allFileIds = set()
 
         for _, root in self._Roots:
-            base = root + path
+            p = root + path
+
             try:
-                names = os.listdir(base)
+                names = os.listdir(p)
+
             except FileNotFoundError:
                 names = []
 
-            objs = set()
+            fIds = set()
             for name in names:
-                st = os.lstat(ospath.join(base, name))
+                stat_ = os.lstat(ospath.join(p, name))
 
-                if stat.S_ISDIR(st.st_mode):
+                if stat.S_ISDIR(stat_.st_mode):
                     r.add(name)
                     continue
 
-                objs.add((name, st.st_mtime, st.st_size))
+                fIds.add((name, stat_.st_mtime_ns, stat_.st_size))
 
-            allObjs |= objs
-            objss.append(objs)
+            fileIds.append(fIds)
+            allFileIds.update(fIds)
 
         r = list(r)
 
-        for obj in allObjs:
-            mask = ''.join(c if obj in objs else '.' for (c, _), objs in zip(self._Roots, objss))
-            r.append('_'.join([mask, obj[0]]))
+        for fileId in allFileIds:
+            name, _, _ = fileId
+
+            mask = ''.join(character if fileId in fIds else '.' for (character, _), fIds in zip(self._Roots, fileIds))
+            name = ''.join([mask, '_', name])
+            r.append(name)
 
         return r
-
-        if False:
-            allNames = set()
-            namess = []
-
-            for _, root in self._Roots:
-                base = root + path
-                try:
-                    names = os.listdir(base)
-                except FileNotFoundError:
-                    namess.append(set())
-                    continue
-
-                names = set(names)
-                allNames |= names
-                namess.append(names)
-
-            r = []
-            for name in allNames:
-                mask = ''.join(c if name in names else '.' for (c, _), names in zip(self._Roots, namess))
-                r.append('_'.join([mask, name]))
-
-            return r
 
     @fuse_errors
     def readlink(self, path):
-        return os.readlink(self._resolve(path)[0][1])
+        paths = self._resolve(path)
+        _, p = paths[0]
+        return os.readlink(p)
 
     @fuse_errors
-    def release(self, path, fh):
-        for fh in self._OpenFileHandles.pop(fh):
+    def release(self, path, fileHandle):
+        for fh in self._OpenFileHandles[fileHandle]:
             r = os.close(fh)
+
+        del self._OpenFileHandles[fileHandle]
+
         return r
 
-    # def releasedir(self, path, fh):
+    # def releasedir(self, path, fh):  # residual from FUSE doc
 
-    # def removexattr(self, path, name): # TODO see getxattr
+    removexattr = None  # TODO see getxattr
 
     @fuse_errors
     def rename(self, old, new):
         '''
-        - like self.link
-        - new without old -> file not found error
-        - rename for existing new first
-        - old without new -> ignored
-        '''
+        Rename file.
 
-        olds = dict(self._resolve(old))
+        Similar to `.link`.
+
+        - if new without old
+          - raises file not found error
+        - for all news
+          - creates parent directories as needed
+          - rename corresponding old to new
+        '''
+        olds = collections.OrderedDict(self._resolve(old))
+
         try:
             news = self._resolve(new)
-        except fuse.FuseOSError:
-            news = {root: root + new for root in olds}
-        else:
-            news = dict(news)
+
+        except fuse.FuseOSError:  # news not specified and don't exist
+            news = ((root, root + new) for root in olds)
+
+        news = collections.OrderedDict(news)
+
         # len(news) != 0
 
-        if len(news.keys() - olds.keys()) != 0:
+        if len(news.keys() - olds.keys()) != 0:  # new without old
             raise fuse.FuseOSError(fuse.ENOENT)
+
         # news is subset of olds
 
-        news = sorted(news.items(), key=lambda item: ospath.exists(item[1]), reverse=True)
-        # existing new are first
-        for root, newPath in news:
+        for root, newPath in reversed(news.items()):
             self._ensure_directory(newPath)
             r = os.rename(olds[root], newPath)
 
@@ -352,17 +350,21 @@ class Mydfs(fuse.LoggingMixIn, fuse.Operations):
 
     @fuse_errors
     def rmdir(self, path):
-        for _, sPath in reversed(self._resolve(path)):
-            r = os.rmdir(sPath)
+        for _, p in reversed(self._resolve(path)):
+            r = os.rmdir(p)
+
         return r
 
-    # def setxattr(self, path, name, value, options, position=0): TODO see getxattr
+    setxattr = None  # TODO see getxattr
 
     @fuse_errors
     def statfs(self, path):
-        stv = os.statvfs(self._resolve(path)[0][1])
+        paths = self._resolve(path, orBestInexistent=True)
+        _, p = paths[0]
+        stat_ = os.statvfs(p)  # from fusepy loopback example
+
         return {
-            key: getattr(stv, key)
+            key: getattr(stat_, key)
             for key in ('f_bavail', 'f_bfree', 'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
                         'f_frsize', 'f_namemax')
         }
@@ -370,59 +372,81 @@ class Mydfs(fuse.LoggingMixIn, fuse.Operations):
     @fuse_errors
     def symlink(self, target, source):
         '''
-        - like self.link
+        Create symbolic link.
+
+        For details see `._link`.
         '''
-        sources = dict(self._resolve(source))
+        return self._link(target, source, os.symlink)
+
+    def _link(self, target, source, linkFunc):
+        '''
+        - if target without source
+          - raises file not found error
+        - if any target exists
+          - raises file exists error
+        - for all targets
+          - creates parent directories as needed
+          - links corresponding source to target
+        '''
+        sources = collections.OrderedDict(self._resolve(source))
 
         try:
             targets = self._resolve(target)
-        except fuse.FuseOSError:
-            targets = {root: root + target for root in sources}
-        else:
-            targets = dict(targets)
+
+        except fuse.FuseOSError:  # targets not specified and don't exist
+            targets = ((root, root + target) for root in sources)
+
+        targets = collections.OrderedDict(targets)
+
         # len(targets) != 0
 
-        if len(targets.keys() - sources.keys()) != 0:
+        if len(targets.keys() - sources.keys()) != 0:  # target without source
             raise fuse.FuseOSError(fuse.ENOENT)
+
         # targets is subset of sources
 
         for _, path in targets:
             if ospath.exists(path):
                 raise fuse.FuseOSError(fuse.EEXIST)
+
         # no target exists
 
-        for root, targetPath in targets.items():
+        for root, targetPath in reversed(targets.items()):
             self._ensure_directory(targetPath)
-            r = os.symlink(sources[root], targetPath)
-        # random order!
+            r = linkFunc(sources[root], targetPath)
 
         return r
 
     @fuse_errors
     def truncate(self, path, length, fh=None):
-        for _, sPath in reversed(self._resolve(path)):
-            with open(sPath, 'r+') as f:
+        for _, p in reversed(self._resolve(path)):
+            # from fusepy loopack example
+            with open(p, 'r+') as f:
                 r = f.truncate(length)
+
         return r
 
     @fuse_errors
     def unlink(self, path):
-        for _, sPath in reversed(self._resolve(path)):
-            r = os.unlink(sPath)
+        for _, p in reversed(self._resolve(path)):
+            r = os.unlink(p)
+
         return r
 
     @fuse_errors
     def utimens(self, path, times=None):
-        for _, sPath in reversed(self._resolve(path)):
-            r = os.utime(sPath, times=times)
+        for _, p in reversed(self._resolve(path)):
+            r = os.utime(p, times=times)
+
         return r
 
     @fuse_errors
-    def write(self, path, data, offset, fh):
-        with self._get_file_handle_lock(fh):
-            for fhi in self._OpenFileHandles[fh]:
-                os.lseek(fhi, offset, 0)
-                r = os.write(fhi, data)
+    def write(self, path, data, offset, fileHandle):
+        with self._get_file_handle_lock(fileHandle):
+            for fh in self._OpenFileHandles[fileHandle]:
+                os.lseek(fh, offset, 0)  # from fusepy loopack example
+                r = os.write(fh, data)
+
         return r
 
     def _resolve(self, path, orBestInexistent=False):
@@ -498,11 +522,12 @@ class Mydfs(fuse.LoggingMixIn, fuse.Operations):
 
         return (root, path)
 
-    def _get_file_handle_lock(self, fh):
+    def _get_file_handle_lock(self, fileHandle):
         with self._FileHandleLock:
-            r = self._FileHandleLocks.get(fh, None)
+            r = self._FileHandleLocks.get(fileHandle, None)
             if r is None:
-                self._FileHandleLocks[fh] = r = threading.Lock()
+                self._FileHandleLocks[fileHandle] = r = threading.Lock()
+
         return r
 
     def _ensure_directory(self, path):
@@ -510,41 +535,32 @@ class Mydfs(fuse.LoggingMixIn, fuse.Operations):
 
 
 if __name__ == '__main__':
-    import sys
+    import argparse
+    import logging
 
-    def printUsage():
-        print('usage: {} [--debug] <c>=<root> [<c>=<root> ...] <mountpoint>'.format(sys.argv[0]))
+    parser = argparse.ArgumentParser()
 
-    args = sys.argv[1:]
+    parser.add_argument('-d', '--debug', action='store_true', default=False, help='Debug mode')
+    parser.add_argument(
+        metavar='root', nargs='+', dest='roots', help='Path of root directory as \'{character}={path}\'')
+    parser.add_argument('dir', help='Path of directory to attach to')
 
-    if '--debug' in args:
-        debug = True
-        args.remove('--debug')
-    else:
-        debug = False
+    args = parser.parse_args()
 
-    if len(args) < 2:
-        print('too few arguments')
-        printUsage()
-        exit(1)
-
-    rootArgs = args[:-1]
     roots = []
-    for i, arg in enumerate(rootArgs):
-        if len(arg) < 3 or arg[1] != '=':
-            print('invalid argument {}'.format(repr(arg)))
-            printUsage()
-            exit(2)
+    for root in args.roots:
+        i = root.find('=')
+        if i == -1:
+            raise argparse.ArgumentError('root', 'Invalid root {}'.format(repr(root)))
 
-        root = arg[2:]
-        if not ospath.isdir(root):
-            print('root either does not exist or is not a directory {}'.format(repr(root)))
-            exit(3)
+        character = root[:i]
+        path = root[(i + 1):]
 
-        roots.append((arg[0], root))
+        if not ospath.isdir(path):
+            raise FileNotFoundError(path)
 
-    mountpoint = args[-1]
+        roots.append((character, path))
 
     logging.basicConfig()
 
-    fuse.FUSE(Mydfs(roots), mountpoint, foreground=True, debug=debug)
+    fuse.FUSE(Mydfs(roots), args.dir, foreground=True, debug=args.debug)
